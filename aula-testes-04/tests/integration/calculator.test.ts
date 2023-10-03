714a6b47e450ef2fbbb3e131bf5080a08922d9a5
Gabriel Zarzana Espinoza <gabriel.zarzana@gmail.com>
2023-10-03 17:14:18 -0300

    Exercise 04

--- aula-testes-04/tests/integration/calculator.test.ts
+++ aula-testes-04/tests/integration/calculator.test.ts
@@ -1,6 +1,5 @@
 import supertest from "supertest";
 import app from "../../src/app";
-import { MathBody } from "../../src/validator";
 
 const api = supertest(app);
 
@@ -23,64 +22,5 @@ describe("calculator tests", () => {
 
     const { status } = await api.post("/math").send(mathBody);
     expect(status).toBe(400);
-  });
-
-  it("should sum two numbers", async () => {
-    const mathBody: MathBody = {
-      operation: "sum",
-      n1: 2,
-      n2: 2
-    };
-
-    const { status, body } = await api.post("/math").send(mathBody);
-    expect(status).toBe(200);
-    expect(body.result).toBe(4);
-  });
-
-  it("should subtract two numbers", async () => {
-    const mathBody: MathBody = {
-      operation: "sub",
-      n1: 2,
-      n2: 2
-    };
-
-    const { status, body } = await api.post("/math").send(mathBody);
-    expect(status).toBe(200);
-    expect(body.result).toBe(0);
-  });
-  it("should multiply two numbers", async () => {
-    const mathBody: MathBody = {
-      operation: "mul",
-      n1: 3,
-      n2: 3
-    };
-
-    const { status, body } = await api.post("/math").send(mathBody);
-    expect(status).toBe(200);
-    expect(body.result).toBe(9);
-  });
-
-  it("should divide two numbers", async () => {
-    const mathBody: MathBody = {
-      operation: "div",
-      n1: 2,
-      n2: 2
-    };
-
-    const { status, body } = await api.post("/math").send(mathBody);
-    expect(status).toBe(200);
-    expect(body.result).toBe(1);
-  });
-
-  it("should return 0 when diving by zero", async () => {
-    const mathBody: MathBody = {
-      operation: "div",
-      n1: 2,
-      n2: 0
-    };
-
-    const { status, body } = await api.post("/math").send(mathBody);
-    expect(status).toBe(200);
-    expect(body.result).toBe(0);
   });
 });
--- aula-testes-04/tests/unit/calculator.test.ts
+++ aula-testes-04/tests/unit/calculator.test.ts
@@ -1,5 +1,58 @@
+import calculator from "calculator";
+import { MathBody } from "../../src/validator";
+
 describe("calculator tests", () => {
-  it("should work", async () => {
-    expect(true).toBe(true);
+  it("should sum two numbers", async () => {
+    const mathBody: MathBody = {
+      operation: "sum",
+      n1: 2,
+      n2: 2
+    };
+
+    const sum = calculator.sum(mathBody.n1, mathBody.n2);
+    expect(sum).toBe(4);
+  });
+
+  it("should subtract two numbers", async () => {
+    const mathBody: MathBody = {
+      operation: "sub",
+      n1: 2,
+      n2: 2
+    };
+
+    const sub = calculator.sub(mathBody.n1, mathBody.n2);
+    expect(sub).toBe(0);
+  });
+  it("should multiply two numbers", async () => {
+    const mathBody: MathBody = {
+      operation: "mul",
+      n1: 3,
+      n2: 3
+    };
+
+    const mul = calculator.mul(mathBody.n1, mathBody.n2);
+    expect(mul).toBe(9);
+  });
+
+  it("should divide two numbers", async () => {
+    const mathBody: MathBody = {
+      operation: "div",
+      n1: 2,
+      n2: 2
+    };
+
+    const div = calculator.div(mathBody.n1, mathBody.n2);
+    expect(div).toBe(1);
+  });
+
+  it("should return 0 when diving by zero", async () => {
+    const mathBody: MathBody = {
+      operation: "div",
+      n1: 2,
+      n2: 0
+    };
+
+    const div = calculator.div(mathBody.n1, mathBody.n2);
+    expect(div).toBe(0);
   });
 })

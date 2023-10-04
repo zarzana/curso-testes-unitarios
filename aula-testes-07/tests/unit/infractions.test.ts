1f680be46b255d5f89a108c744a22f67d76a7529
Gabriel Zarzana Espinoza <gabriel.zarzana@gmail.com>
2023-10-04 17:52:43 -0300

    Exercise 07

--- aula-testes-07/tests/unit/infractions.test.ts
+++ aula-testes-07/tests/unit/infractions.test.ts
@@ -1,11 +1,19 @@
-describe("Infractions Service Tests", () => {
-  it("should get infractions from user", async () => {
-    // TODO
-    expect(true).toBe(true);
+import { faker } from "@faker-js/faker";
+import * as usersRepository from "../../src/users-repository";
+import { getInfractionsFrom } from "infractions-service";
+import { User } from "@prisma/client";
+
+describe("Infractions Service", () => {
+  it("should get infractions", async () => {
+    const userData: User = { id: 1, firstName: faker.person.firstName(), lastName: faker.person.lastName(), licenseId: '111.111.11.11' };
+    jest.spyOn(usersRepository, "getUserByDocument").mockImplementationOnce((): any => { return { userData }; });
+    const infractions = await getInfractionsFrom('111.111.11.11');
+    expect(infractions).toEqual({ userData, infractions: [] })
   });
 
-  it("should throw an error when driver license does not exists", () => {
-    // TODO
-    expect(true).toBe(true);
-  })
+  it("should throw error when driver license is invalid", () => {
+    jest.spyOn(usersRepository, "getUserByDocument").mockImplementationOnce((): any => { return undefined; });
+    const promise = getInfractionsFrom('1');
+    expect(promise).rejects.toEqual({ type: "NOT_FOUND", message: "Driver not found." });
+  });
 });

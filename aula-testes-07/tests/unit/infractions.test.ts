b2657cf92358d208421c15df415078274786e883
Gabriel Zarzana Espinoza <gabriel.zarzana@gmail.com>
2023-10-04 17:50:33 -0300

    Exercise 07 template

---
+++ aula-testes-07/tests/unit/infractions.test.ts
@@ -0,0 +1,11 @@
+describe("Infractions Service Tests", () => {
+  it("should get infractions from user", async () => {
+    // TODO
+    expect(true).toBe(true);
+  });
+
+  it("should throw an error when driver license does not exists", () => {
+    // TODO
+    expect(true).toBe(true);
+  })
+});

e458531b5607e547a3a9827636ac16b6992c745e
Gabriel Zarzana Espinoza <gabriel.zarzana@gmail.com>
2023-10-04 17:57:04 -0300

    Exercise 06

--- aula-testes-06/tests/unit/orders.test.ts
+++ aula-testes-06/tests/unit/orders.test.ts
@@ -4,19 +4,24 @@ import { createOrder, getOrderByProtocol } from "../../src/order-service";
 import * as orderRepository from "../../src/order-repository";
 import { OrderInput } from "../../src/validator";
 
-describe("Order Service Tests", () => {
-  it("should create an order", async () => {
-    // TODO
-    expect(true).toBe(true);
+describe("Order Service", () => {
+  it("should create order", async () => {
+    const orderData: OrderInput = { client: faker.person.fullName(), description: faker.commerce.product() };
+    const order = await createOrder(orderData);
+    expect(order).toEqual({ protocol: expect.any(String), status: "IN_PREPARATION" });
   });
 
-  it("should return an order based on the protocol", async () => {
-    // TODO
-    expect(true).toBe(true);
+  it("should return order based on protocol", async () => {
+    const protocolData: { protocol: string, status: string } = { protocol: new Date().getTime().toString(), status: "IN_PREPARATION" };
+    jest.spyOn(orderRepository, "getByProtocol").mockImplementationOnce((): any => { return protocolData });
+    const order = await getOrderByProtocol(protocolData.protocol);
+    expect(order).toEqual(protocolData);
   });
 
-  it("should return status INVALID when protocol doesn't exists", async () => {
-    // TODO
-    expect(true).toBe(true);
+  it("should return INVALID as status when protocol does not exist", async () => {
+    jest.spyOn(orderRepository, "getByProtocol").mockImplementationOnce((): any => { return undefined });
+    const nonExistentProtocol = '1234567890';
+    const order = await getOrderByProtocol(nonExistentProtocol);
+    expect(order).toEqual({ protocol: nonExistentProtocol, status: 'INVALID' });
   });
 });

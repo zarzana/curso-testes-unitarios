b7d37b87f51b6a7b1044041840682c21f072f625
Gabriel Zarzana Espinoza <gabriel.zarzana@gmail.com>
2023-10-04 17:44:15 -0300

    Exercise 05

--- aula-testes-05/tests/unit/pacientes.test.ts
+++ aula-testes-05/tests/unit/pacientes.test.ts
@@ -1,5 +1,18 @@
-describe("calculator tests", () => {
-  it("should work", async () => {
-    expect(true).toBe(true);
+import { generateProtocolForPacient } from "protocols-generator";
+
+jest.mock("uuid", () => {
+  return { v4: () => { return "valor simulado no mock" } }
+});
+
+describe("Protocol generation", () => {
+  it("should return a protocol", async () => {
+    const protocolData: { name: string, lastName: string, priority: boolean } = { name: 'Bob', lastName: 'Bobson', priority: true };
+    const protocol = generateProtocolForPacient(protocolData.name, protocolData.lastName, protocolData.priority);
+    expect(protocol).toEqual({
+      priority: protocolData.priority,
+      date: expect.any(Date),
+      pacient: `${protocolData.name} ${protocolData.lastName}`,
+      protocol: expect.any(String),
+    });
   });
 });
